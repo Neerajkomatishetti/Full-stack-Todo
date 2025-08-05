d60277465ead3dd869cf8423ea7033c95e397ff8
neeraj <neerajkomatishetti@gmail.com>
2025-08-05 16:14:28 +0530

    changes for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState , useEffect } from 'react'
 import './App.css'
 import {CreateTodo} from './CreateTodo.jsx'
 import {Todo} from './Todo.jsx'
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
   const [todos, setTodos] = useState([]);
