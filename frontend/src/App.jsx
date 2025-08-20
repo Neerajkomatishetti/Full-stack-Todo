21328a2fb4c51b838171065312747356f1ceb66c
neeraj <neerajkomatishetti@gmail.com>
2025-08-21 00:41:48 +0530

    added spinner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,30 +4,22 @@ import {CreateTodo} from './CreateTodo.jsx'
 import {Todo} from './Todo.jsx'
 import {Login} from './components/login/login.jsx'
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+import { SpinnerCircular } from 'spinners-react';
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  // const fetchTodos = () => {
-  //   fetch(`${API_URL}/todos`)
-  //     .then(async (res) => {
-  //       const response = await res.json();
-  //       setTodos(response.Todos);
-  //     });
-  // };
+  const fetchTodos = () => {
+    fetch(`${API_URL}/todos`)
+      .then(async (res) => {
+        const response = await res.json();
+        setTodos(response.Todos);
+        setLoading(false);
+      });
+  };
 
   useEffect(() => {
-    const fetchTodos = async () => {
-      try {
-        const res = await fetch(`${API_URL}/todos`);
-        const response = await res.json();
-        setLoading(false);
-        setTodos(response.Todos);
-      } catch (error) {
-        console.error("Failed to fetch todos:", error);
-      }
-};
     fetchTodos();
     console.log(API_URL)
   }, []);
@@ -35,8 +27,9 @@ function App() {
   return (
     <div className='app-container'>
       <CreateTodo fetchTodos={fetchTodos} />
-      {loading? <div>loading...</div> : <Todo todos = {todos} fetchTodos={fetchTodos}/>}
-      {/* <Login/> */}
+      {loading?(
+  <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <SpinnerCircular /></div>) : (<Todo todos = {todos} fetchTodos={fetchTodos}/>)}
     </div>
   )
 }
